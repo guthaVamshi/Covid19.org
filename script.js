1e4c8602c634d44206d44ba7ffa273852e60ceb7
Vamshigutha <79559118+guthaVamshi@users.noreply.github.com>
2021-09-07 12:54:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 
 // api url
-var Names, data, DateToday, PreYesterday, DayBackYesterday, StatesData, StateName, StateAbb; var NewCases = 0; var sum = 0; var CasesTotal = 0;
+var Names, CasesJson, DateToday, PreYesterday, DayBackYesterday,PartiallyVaccinatedPercentage, population,StatesData, StateName, StateAbb; var NewCases = 0; var sum = 0; var CasesTotal = 0;
 var val = new Array();
 const api_url =
 	"https://data.covid19india.org/v4/min/timeseries.min.json";
@@ -14,10 +14,10 @@ async function getapi(url) {
 	// Storing response
 	const response = await fetch(url);
 
-	// Storing data in form of JSON
-	data = await response.json();
+	// Storing CasesJson in form of JSON
+	CasesJson = await response.json();
 
-	Names = Object.keys(data);
+	Names = Object.keys(CasesJson);
 
 
 	if (response) {
@@ -34,9 +34,33 @@ async function getRes(url) {
 	StateName = Object.values(StatesData);
 	StateAbb = Object.keys(StatesData);
 
-	show(data);
-	LogData();
+	
+	getPop();
+	
+	
 };
+async function getPop(){
+	var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "country": "india"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+const res = await fetch("https://countriesnow.space/api/v0.1/countries/population", requestOptions);
+result = await res.json();
+ 
+	Lastpop = Object.values(result['data']['populationCounts'])[Object.values(result['data']['populationCounts']).length - 1];
+	population = Lastpop['value'];
+  show(CasesJson);
+}; 
 
 getapi(api_url);
 
@@ -56,21 +80,21 @@ function show() {
 	// Loop to access all rows
 	for (let i = 0; i < 38; i++) {
 		DateToday = new Date();
-		PreYesterday = Object.keys(data[Names[i]]['dates'])[Object.keys(data[Names[i]]['dates']).length - 2];
-		DayBackYesterday = Object.keys(data[Names[i]]['dates'])[Object.keys(data[Names[i]]['dates']).length - 1];
+		PreYesterday = Object.keys(CasesJson[Names[i]]['dates'])[Object.keys(CasesJson[Names[i]]['dates']).length - 2];
+		DayBackYesterday = Object.keys(CasesJson[Names[i]]['dates'])[Object.keys(CasesJson[Names[i]]['dates']).length - 1];
 
-		if (data[Names[i]]['dates'][DayBackYesterday]) {
+		if (CasesJson[Names[i]]['dates'][DayBackYesterday]) {
 			if (i == 34 || i ==33) {
 				continue;
 
 			}
-			CasesTotal = data[Names[i]]['dates'][DayBackYesterday]['total']['confirmed'];
-			Deceased = data[Names[i]]['dates'][DayBackYesterday]['total']['deceased'];
-			Recovered = data[Names[i]]['dates'][DayBackYesterday]['total']['recovered'];
+			CasesTotal = CasesJson[Names[i]]['dates'][DayBackYesterday]['total']['confirmed'];
+			Deceased = CasesJson[Names[i]]['dates'][DayBackYesterday]['total']['deceased'];
+			Recovered = CasesJson[Names[i]]['dates'][DayBackYesterday]['total']['recovered'];
 			
 			active = CasesTotal - (Deceased + Recovered);
 			
-			NewCases = ((data[Names[i]]['dates'][DayBackYesterday]['total']['confirmed']) - (data[Names[i]]['dates'][PreYesterday]['total']['confirmed']));
+			NewCases = ((CasesJson[Names[i]]['dates'][DayBackYesterday]['total']['confirmed']) - (CasesJson[Names[i]]['dates'][PreYesterday]['total']['confirmed']));
 		
 		
 		} 
@@ -108,21 +132,24 @@ function show() {
 
 	// Setting innerHTML as tab variable
 	document.getElementById("DataTable").innerHTML = tab;
+	
 DomUpdate();
 	
 }
 
 function DomUpdate(){
-	LastDayOfTotalCases = Object.keys(data[Names[33]]['dates'])[Object.keys(data[Names[33]]['dates']).length - 1];
-	DayBeforeOfTotalCases = Object.keys(data[Names[33]]['dates'])[Object.keys(data[Names[33]]['dates']).length - 2];
-	TotalCases = data[Names[33]]['dates'][LastDayOfTotalCases]['total']['confirmed'];
-	DeceasedCases = data[Names[33]]['dates'][LastDayOfTotalCases]['total']['deceased'];
-	RecoveredCases = data[Names[33]]['dates'][LastDayOfTotalCases]['total']['recovered'];
+	LastDayOfTotalCases = Object.keys(CasesJson[Names[33]]['dates'])[Object.keys(CasesJson[Names[33]]['dates']).length - 1];
+	DayBeforeOfTotalCases = Object.keys(CasesJson[Names[33]]['dates'])[Object.keys(CasesJson[Names[33]]['dates']).length - 2];
+	TotalCases = CasesJson[Names[33]]['dates'][LastDayOfTotalCases]['total']['confirmed'];
+	DeceasedCases = CasesJson[Names[33]]['dates'][LastDayOfTotalCases]['total']['deceased'];
+	RecoveredCases = CasesJson[Names[33]]['dates'][LastDayOfTotalCases]['total']['recovered'];
 	ActiveCases = TotalCases - (DeceasedCases + RecoveredCases);
-	vaccinated1 = data[Names[33]]['dates'][DayBackYesterday]['total']['vaccinated1'];
-
-	vaccinated2 = data[Names[33]]['dates'][DayBackYesterday]['total']['vaccinated2'];
+	vaccinated1 = CasesJson[Names[33]]['dates'][DayBackYesterday]['total']['vaccinated1'];
+	PartiallyVaccinatedPercentage = (vaccinated1/population)*100;
 	
+	
+	vaccinated2 = CasesJson[Names[33]]['dates'][DayBackYesterday]['total']['vaccinated2'];
+	FullyVaccinatedPercentage = (vaccinated2/population)*100;
 	TotalVaccines = vaccinated1 + vaccinated2;
 	document.getElementById('Confirmed').innerHTML = TotalCases.toLocaleString('en-IN');
 	document.getElementById('Active').innerHTML = ActiveCases.toLocaleString('en-IN');
@@ -130,11 +157,14 @@ function DomUpdate(){
 	document.getElementById('Deceased').innerHTML = DeceasedCases.toLocaleString('en-IN');
 	document.getElementById('DateId').innerHTML = DateToday.toLocaleString('en-IN');
 	document.getElementById('Vaccines').innerHTML += TotalVaccines.toLocaleString('en-IN');
-	document.getElementById('NewCases').innerHTML += ((TotalCases) - (data[Names[33]]['dates'][DayBeforeOfTotalCases]['total']['confirmed'])).toLocaleString('en-IN');
-	document.getElementById('NewRecovered').innerHTML += ((RecoveredCases) - (data[Names[33]]['dates'][DayBeforeOfTotalCases]['total']['recovered'])).toLocaleString('en-IN');
-	document.getElementById('NewDeceased').innerHTML += ((DeceasedCases) - (data[Names[33]]['dates'][DayBeforeOfTotalCases]['total']['deceased'])).toLocaleString('en-IN');
+	document.getElementById('NewCases').innerHTML += ((TotalCases) - (CasesJson[Names[33]]['dates'][DayBeforeOfTotalCases]['total']['confirmed'])).toLocaleString('en-IN');
+	document.getElementById('NewRecovered').innerHTML += ((RecoveredCases) - (CasesJson[Names[33]]['dates'][DayBeforeOfTotalCases]['total']['recovered'])).toLocaleString('en-IN');
+	document.getElementById('NewDeceased').innerHTML += ((DeceasedCases) - (CasesJson[Names[33]]['dates'][DayBeforeOfTotalCases]['total']['deceased'])).toLocaleString('en-IN');
 	document.getElementById('VaccinatedDose1').innerHTML += vaccinated1.toLocaleString('en-IN');
 	document.getElementById('VaccinatedDose2').innerHTML += vaccinated2.toLocaleString('en-IN');
+	document.getElementById('PercentageOfPartiallyVaccinated').innerHTML += PartiallyVaccinatedPercentage.toFixed(2)+'%';
+	document.getElementById('PercentageOfFullyVaccinated').innerHTML += FullyVaccinatedPercentage.toFixed(2)+'%';
+
 };
 function hideloader() {
 	document.getElementById('loading').style.display = 'none';
